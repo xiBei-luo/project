e87f95788b1255bbf60e552670ba03d5574d4c95
xiBei-luo <1271826981@qq.com>
2017-08-03 08:56:25 +0800

    淘宝网首页+js选项卡

--- 淘宝网首页/js/main.js
+++ 淘宝网首页/js/main.js
@@ -1,5 +1,21 @@
+window.onload=function(){
+    var oUl=document.getElementById('u_tab_ele');
+    var aTab=oUl.getElementsByTagName('li');
+    var oCont=document.getElementById('u_tab_item');
+    var aDatas=['最受赞赏公司阿里折桂1','最受赞赏公司阿里折桂2','最受赞赏公司阿里折桂3','最受赞赏公司阿里折桂4','最受赞赏公司阿里折桂5']
 
-
+    for(var i=0;i<aTab.length;i++){
+        aTab[i].index=i;
+        aTab[i].onclick=function(){
+            for(var i=0;i<aTab.length;i++){
+                aTab[i].className='';
+                oCont.innerHTML='';
+            }
+            this.className='current';
+            oCont.innerHTML="<li>"+aDatas[this.index]+"</li>"+"<li>"+aDatas[this.index]+"</li>"+"<li>"+aDatas[this.index]+"</li>"+"<li>"+aDatas[this.index]+"</li>"
+        }
+    }
+}
 
 
 
